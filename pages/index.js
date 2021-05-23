901d92c4a2673df1b1f2460a0c42850230e33ef2
Darshan Satya <darshans1989@gmail.com>
2021-05-23 11:07:27 -0400

    mapping pages and components

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A first Meetup",
+    image:
+      "https://images.unsplash.com/photo-1524178232363-1fb2b075b655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80",
+    address: "928302 - #283 District",
+    description: "Test Description",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://images.unsplash.com/photo-1524178232363-1fb2b075b655?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1350&q=80",
+    address: "928302 - #283 District",
+    description: "Test Description",
+  },
+];
+
+const HomePage = () => {
+  return (
+      <MeetupList meetups={DUMMY_MEETUPS} />
+  );
+};
+
+export default HomePage;
