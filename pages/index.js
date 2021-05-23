d3c229924084f389819eff74646376b513ae3aaa
Darshan Satya <darshans1989@gmail.com>
2021-05-23 11:39:48 -0400

    getStatic props

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -19,10 +20,17 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-const HomePage = () => {
-  return (
-      <MeetupList meetups={DUMMY_MEETUPS} />
-  );
+const HomePage = (props) => {
+  return <MeetupList meetups={props.meetups} />;
 };
+
+export async function getStaticProps() {
+  // fetch data from API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
+}
 
 export default HomePage;
